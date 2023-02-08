46cacf1201c99814c802984e5eb89b67c7884772
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-08 22:09:26 +0300

    add function Task25_Stepen

---
+++ Task25_Stepen/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную 
+// степень B.
+
+int IntInput(string intName) // функция принимает число из консоли, преобразуя его в целое
+{
+    int num;
+
+    Console.Write($"Введите число {intName}: ");
+    while(!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Ерунда какая-то... Ощущение, что введено не целое число.");
+        Console.Write($"Ну-ка, еще разок! {intName} = ");
+    }
+    return  num;
+}
+
+// сама программа
+
+int numberA;
+int numberB;
+double res = 1;
+
+Console.WriteLine();
+Console.WriteLine();
+
+numberA = IntInput("A");
+numberB = IntInput("B");
+
+for(int i = 1; i <= numberB; i++)
+{
+    res = res * numberA;
+}
+
+Console.WriteLine("-------------------------------------------------------------");
+Console.WriteLine($"Число {numberA} в степени {numberB} равно: {res}");
+Console.WriteLine();
+Console.WriteLine();
