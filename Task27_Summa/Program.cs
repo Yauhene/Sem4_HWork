4b8f97fbab8e6df7b45f7ba45a0ef96b4efd848f
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-08 23:31:04 +0300

    Task27_Summa complit

---
+++ Task27_Summa/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int IntInput(string intName) // функция принимает число из консоли, преобразуя его в целое
+{
+    int num;
+    Console.Write("Введите число : ");
+
+    while(!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Ерунда какая-то... Ощущение, что введено не целое число.");
+        Console.Write($"Ну-ка, еще разок! {intName} = ");
+    }
+    return  num;
+}
+
+int sumOfDigits(int inp)
+{
+    int numRes = 0;
+    //int tempNum = inp;
+    while (inp > 0)
+    {
+        //tempNum = inp % 10;
+        numRes = numRes + (inp % 10);
+        //tempNum = 
+        inp = inp / 10;
+    }
+    return numRes;
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+int number = IntInput("");
+int totalSum = sumOfDigits(number);
+
+Console.WriteLine("-------------------------------------------------------------");
+Console.WriteLine($"Сумма цифр числа {number} равна {totalSum}");
+Console.WriteLine();
+Console.WriteLine();
